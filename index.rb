ed06770e1fd1f418a55938a4e6b86844e628811f
Joe Burns <joseph.burns@gmail.com>
2024-01-18 14:55:21 -0600

    WIP

--- index.rb
+++ index.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+require 'sinatra/namespace'
 require 'evernote_oauth'
 
 CLIENT_KEY = 'goodguylaskygpt-3523'
@@ -10,15 +11,22 @@ if !defined?(Fixnum)
   Fixnum = Integer
 end
 
+enable :sessions
+set :session_secret, '92c4ecd3d5863023d40ef6da1630b50c3d376f81868b419355e599eb6e86e5e9f9c6d56c29e3e59cbc0f80cacfab879bfa2d9e959f4d7970e5cce41ca5aab704'
 
 get '/' do
+  erb :index
+end
+
+get '/auth' do
   client = EvernoteOAuth::Client.new(
     consumer_key: CLIENT_KEY,
     consumer_secret: CLIENT_SECRET,
     sandbox: SANDBOX
   )
 
-  request_token = client.request_token(:oauth_callback => 'http://localhost:4567/callback')
+  base_url = "http://localhost:4567"
+  request_token = client.request_token(:oauth_callback => "#{base_url}/callback")
   session[:request_token] = request_token
 
   redirect request_token.authorize_url
@@ -26,25 +34,74 @@ end
 
 get '/callback' do
   request_token = session[:request_token]
-  @access_token = request_token.get_access_token(:oauth_verifier => params[:oauth_verifier])
+  access_token = request_token.get_access_token(:oauth_verifier => params[:oauth_verifier])
+
+  session[:access_token] = access_token.token
+  session[:access_token_secret] = access_token.secret
 
   # Redirect to the notes listing page
   redirect to('/notes')
 end
 
+namespace '/api' do
+  get '/notes' do
+    page_size = params[:page_size]&.to_i || 10
+    page_number = params[:page_number]&.to_i || 1
+    offset = (page_number - 1) * page_size
 
-get '/notes' do
-  oauth_token = 'goodguylaskygpt-3523.18D0332E7C1.687474703A2F2F6C6F63616C686F73743A343536372F63616C6C6261636B.43D474EB12CFDD62B0DEFEC8D54F8A07'
-  oauth_verifier='67398EAB36620C8F0440B9A340E6A0A5&sandbox_lnb=false'
+    oauth_token = session[:access_token]
+    client = EvernoteOAuth::Client.new(token: oauth_token, sandbox:SANDBOX)
 
-  client = EvernoteOAuth::Client.new(token: oauth_token, consumer_key:CLIENT_KEY, sandbox:SANDBOX)
+    note_store = client.note_store
 
-  note_store = client.note_store
+    # Fetch notes
+    filter = Evernote::EDAM::NoteStore::NoteFilter.new
+    notes = note_store.findNotes(filter, offset, page_size).notes
 
-  # Fetch notes
-  filter = Evernote::EDAM::NoteStore::NoteFilter.new
-  notes = note_store.findNotes(filter, 0, 10).notes
+    # Render notes title and guid to JSON
+    response = notes.map do |note|
+      { title: note.title, guid: note.guid }
+    end
 
-  # Render notes
-  erb :index, locals: { notes: notes }
+    # content_type :json
+    response.to_json
+  end
+
+  get '/notes/search' do
+    query = params[:q]
+
+    oauth_token = session[:access_token]
+    client = EvernoteOAuth::Client.new(token: oauth_token, sandbox:SANDBOX)
+
+    note_store = client.note_store
+
+    # Create a NoteFilter with the search query
+    filter = Evernote::EDAM::NoteStore::NoteFilter.new
+    filter.words = query
+
+    # Fetch notes
+    notes = note_store.findNotes(filter, 0, 100).notes
+
+    # Render notes title and guid to JSON
+    notes.map do |note|
+      { title: note.title, guid: note.guid }
+    end.to_json
+  end
+
+
+  get '/notes/:guid' do
+    oauth_token = session[:access_token]
+    client = EvernoteOAuth::Client.new(token: oauth_token, sandbox:SANDBOX)
+
+    note_store = client.note_store
+
+    # Fetch note by guid
+    note = note_store.getNote(params[:guid], true, false, false, false)
+
+    # Render note metadata and content to JSON
+    response = { title: note.title, guid: note.guid, content: note.content }
+
+    # content_type :json
+    response.to_json
+  end
 end
